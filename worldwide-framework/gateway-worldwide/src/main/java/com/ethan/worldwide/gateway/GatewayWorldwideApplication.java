9d2c8e819b86c25c8d24a95af37e6d50991a1217
zhenghui <15270855286@163.com>
2022-07-13 07:36:20 +0800

    网关初始化

---
+++ worldwide-framework/gateway-worldwide/src/main/java/com/ethan/worldwide/gateway/GatewayWorldwideApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.worldwide.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayWorldwideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayWorldwideApplication.class, args);
+    }
+
+}
